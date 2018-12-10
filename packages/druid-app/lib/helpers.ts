4b178a6043e72aca49ecca3b2e82965b54ef4168
Alid Castano <alidcastano@gmail.com>
2018-12-10 18:23:49 -0500

    fix withQueries helper

--- packages/druid-app/lib/helpers.ts
+++ packages/druid-app/lib/helpers.ts
@@ -1,12 +1,12 @@
-import { QueryBuilder } from 'objection'
-
 export function	withQueries (queries) {
-	class CustomQB extends QueryBuilder<any> {}
-	Object.keys(queries).forEach(query => CustomQB.prototype[query] = queries[query])
+	return (Model): typeof Model => {
+		class CustomQB extends Model.QueryBuilder<any> {}
+		Object.keys(queries).forEach(query => CustomQB.prototype[query] = queries[query])
 
-	return (Model): typeof Model => class extends Model {
-		static get QueryBuilder() {
-			return CustomQB
+		return class extends Model {
+			static get QueryBuilder() {
+				return CustomQB
+			}
 		}
 	}
 }
